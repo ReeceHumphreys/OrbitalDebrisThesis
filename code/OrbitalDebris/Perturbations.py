5089cd9ed409da1e066955d197018245f53d22fc
Reece <reecewh@icloud.com>
2021-04-08 20:52:02 -0400

    Finished ch4 started ch5

---
+++ code/OrbitalDebris/Perturbations.py
@@ -0,0 +1,254 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numba import njit as jit, prange
+from numpy import pi, sin, cos, sqrt
+from scipy import integrate
+from scipy.special import iv
+
+# User defined libearayr
+import planetary_data as pd
+import CoordTransforms as ct
+import Aerodynamics as aero
+
+def null_perts():
+    return {
+        'J2': False,
+        'aero': False,
+        'moon_grav': False,
+        'solar_grav': False
+    }
+
+class OrbitPropagator:
+
+    def __init__(self, states0, A, M, tspan, dt, rv=False, cb=pd.earth, perts=null_perts()):
+        
+        # Need to add support for initializing with radius and velocity
+        if rv:
+            self.states = 0
+
+        else:
+            self.states = states0
+
+        # Setting the areas and masses
+        self.A = A
+        self.M = M
+
+        # Integration information
+        self.tspan = tspan
+        self.dt = dt
+
+        # Central body properties
+        self.cb = cb
+
+        # Defining perturbations being considered
+        self.perts = perts
+        
+        # Defining constants for aerodynamic drag
+        if self.perts['aero']:
+            self.K_a = np.matrix([[1, 0, 0, 0 ,0 ,0, 0],
+                   [0, 2, 0, 0, 0, 0, 0],
+                   [3/4, 0, 3/4, 0, 0, 0, 0],
+                   [0, 3/4, 0, 1/4, 0, 0, 0],
+                   [21/64, 0, 28/64, 0, 7/64, 0, 0],
+                   [0, 30/64, 0, 15/64, 0, 3/64, 0]])
+
+            self.K_e = np.matrix([[0, 1, 0, 0, 0, 0, 0],
+                   [1/2, 0 , 1/2, 0, 0, 0, 0],
+                   [0, -5/8, 0, 1/8, 0, 0, 0],
+                   [-5/16, 0, -4/16, 0, 1/16, 0, 0],
+                   [0, -18/128, 0, -1/128, 0, 3/128, 0],
+                   [-18/256, 0, -19/256, 0, 2/256, 0, 3/256]])
+
+    def cartesian_representation(self):
+    # Returns the cartesian state representation of states for vis. purposes
+        N_t = self.states.shape[0]
+        N_frag = self.states.shape[2]
+        cartesian_states = np.empty(shape=(N_t, 2, N_frag, 3))
+
+        for i in prange(self.states.shape[0]):
+            cartesian_states[i, :, :] = ct.coe2rv_many_new(self.states[i, :, :])
+
+        return cartesian_states
+
+
+    def diffy_q(self, t, state):
+        e, a, i, Omega, omega = state.reshape(5, len(self.A))
+        N_f = len(self.A)
+
+        # Central body information
+        mu     = self.cb['mu']
+        radius = self.cb['radius'] #[m]
+        J2 = self.cb['J2']
+        
+        # Local variables
+        delta_e = np.zeros_like(e)
+        delta_a = np.zeros_like(a)
+        delta_i = np.zeros_like(i)
+        delta_Omega = np.zeros_like(Omega)
+        delta_omega = np.zeros_like(omega)
+        
+
+        # Current orbital information
+        peri = a * (1 - e) #[m]
+        p    = a * (1 - e**2) #[m] (Semi parameter)
+        n    = np.sqrt(mu / a**3) # (Mea motion)
+
+        ############### Drag effects ############### 
+        if self.perts['aero']:
+            h_p = (peri - radius) #[m]
+            rho = aero.atmosphere_density(h_p/1e3) #[kg * m^-3]
+            H = aero.scale_height(h_p/1e3) * 1e3 #[m]
+
+            z = a*e / H
+            Cd = 0.7
+            tilt_factor =1
+            delta = Cd * (self.A[0] * tilt_factor) / self.M[0]
+
+            e_T = np.array([np.ones_like(e), e, e**2, e**3, e**4, e**5])
+            I_T = np.array([ iv(i, z) for i in range(7)])
+            k_a = delta * np.sqrt(mu * a) * rho
+            k_e = k_a / a
+
+            delta_e = np.zeros_like(e)
+            delta_a = np.zeros_like(a)
+
+            # CASE e < 0.001
+            delta_e = np.zeros_like(e)
+            delta_a = -k_a
+
+            # CASE e>= 0.001
+            I = e>= 0.001
+            trunc_err_a = a[I]**2 * rho[I] * np.exp(-z[I]) * iv(0, z[I]) * e[I]**6
+            trunc_err_e = a[I] * rho[I] * np.exp(-z[I]) * iv(1, z[I]) * e[I]**6
+
+            transform_e = e_T.T.dot(self.K_e) * I_T
+            coef_e = np.array([ transform_e[i,i] for i in range(N_f)])[I]
+
+            transform_a = e_T.T.dot(self.K_a) * I_T
+            coef_a = np.array([ transform_a[i,i] for i in range(N_f)])[I]
+
+            delta_e[I] = -k_e[I] * np.exp(-z[I]) * (coef_e + trunc_err_e)
+            delta_a[I] = -k_a[I] * np.exp(-z[I]) * (coef_a + trunc_err_a)
+
+            delta_e[np.isnan(delta_e)] = 0
+            delta_a[np.isnan(delta_a)] = 0
+            
+            # Deorbit check
+            J = h_p < 100*1e3
+            delta_a[J] = 0
+            delta_e[J] = 0
+    
+        ###############  J2 effects  ############### 
+        if self.perts['J2']:
+            base = (3/2) * self.cb['J2'] * (radius**2/p**2) * n
+            i = np.deg2rad(i)
+            delta_omega = base * (2 - (5/2)*np.sin(i)**2)
+            delta_Omega = -base * np.cos(i)
+            delta_omega = np.rad2deg(delta_omega) % 360
+            delta_Omega = np.rad2deg(delta_Omega) % 360
+
+        return np.concatenate((delta_e, delta_a, delta_i, delta_Omega, delta_omega))
+
+    # Performing a regular propagation, i.e. w/ perturbations
+    def propagate_perturbations(self):
+        
+        # Initial states
+        a0, e0, i0, Omega0, omega0 = self.states[-1, :5, :]
+        y0  = np.concatenate((e0, a0, i0, Omega0, omega0))
+        
+        # Propagation time
+        T_avg = np.mean(self.states[-1, 8, :])
+        times = np.arange(self.tspan[0], self.tspan[-1], self.dt)
+        output = integrate.solve_ivp(self.diffy_q, self.tspan, y0, t_eval = times)
+
+        # Unpacking output (Need to drop first timestep as sudden introduction of drag causes discontinuities)
+        N_f = len(self.A)
+        de = output.y[0:N_f, 1:]
+        da = output.y[N_f:2*N_f, 1:]
+        di = output.y[2*N_f:3*N_f, 1:]
+        dOmega = output.y[3*N_f:4*N_f, 1:]
+        domega = output.y[4*N_f:, 1:]
+        dnu = np.random.uniform(low=0., high=360., size=domega.shape)
+        dp = da * (1 - de**2)
+        
+        # Results
+        return de, da, di, dOmega, domega, dnu, dp
+    
+    # Performing a Keplerian propagation, i.e. w/o perturbations
+    def propagate_orbit(self):
+
+        times      = np.arange(self.tspan[0], self.tspan[-1], self.dt)
+
+        # Mean anomaly rate of change
+        M_dt       = sqrt(self.cb['mu']/self.states[0, :]**3)
+
+        Nd         = len(M_dt)
+        Nt         = len(times)
+
+        # Mean anomaly over time
+        M_t        = np.deg2rad(self.states[5, :, None]) + M_dt[:, None] * times[None, :]
+        M_t        = np.rad2deg(np.mod(M_t, 2*pi))
+
+        # Eccentric anomaly over time. Note need to use E_t in rad, thus convert to deg after using it in
+        # x1 and x2
+        E_t        = np.empty(shape=(Nd, Nt), dtype=np.float32)
+        E_t        = M2E(self.states[1], np.deg2rad(M_t))
+
+        x1         = sqrt(1 + self.states[1, :])[:, None] * sin(E_t / 2)
+        x2         = sqrt(1 - self.states[1, :])[:, None] * cos(E_t / 2)
+        E_t        = np.rad2deg(E_t)
+
+        # True anomaly over time
+        nu_t       = (2*np.arctan2(x1, x2) % (2*pi))
+        nu_t       = np.rad2deg(nu_t).T
+
+        n_times    = nu_t.shape[0]
+        states     = np.empty(shape = (n_times, self.states.shape[0], self.states.shape[1]))
+
+        for i in prange(n_times):
+            state = self.states.copy()
+            state[6, :] = nu_t[i, :]
+            states[i] = state
+        
+        # Update internal states
+        self.states = states
+
+
+
+# Modified from OrbitalPy.utilities
+@jit(parallel=True, fastmath=True)
+def M2E(e_deb, M_t, tolerance=1e-14):
+#Convert mean anomaly to eccentric anomaly.
+#Implemented from [A Practical Method for Solving the Kepler Equation][1]
+#by Marc A. Murison from the U.S. Naval Observatory
+#[1]: http://murison.alpheratz.net/dynamics/twobody/KeplerIterations_summary.pdf
+    n_deb = M_t.shape[0]
+    n_times = M_t.shape[1]
+
+    E_t = np.empty_like(M_t)
+
+    for i in prange(n_deb):
+        e = e_deb[i]
+        for j in prange(n_times):
+            M = M_t[i, j]
+
+            MAX_ITERATIONS = 100
+            Mnorm = np.mod(M, 2 * pi)
+            E0 = M + (-1 / 2 * e ** 3 + e + (e ** 2 + 3 / 2 * cos(M) * e ** 3) * cos(M)) * sin(M)
+            dE = tolerance + 1
+            count = 0
+            while dE > tolerance:
+                t1 = cos(E0)
+                t2 = -1 + e * t1
+                t3 = sin(E0)
+                t4 = e * t3
+                t5 = -E0 + t4 + Mnorm
+                t6 = t5 / (1 / 2 * t5 * t4 / t2 + t2)
+                E = E0 - t5 / ((1 / 2 * t3 - 1 / 6 * t1 * t6) * e * t6 + t2)
+                dE = np.abs(E - E0)
+                E0 = E
+                count += 1
+                if count == MAX_ITERATIONS:
+                    print('Did not converge, increase number of iterations')
+            E_t[i, j] = E
+    return E_t
---
+++ code/OrbitalDebris/generate_debris.py
@@ -0,0 +1,407 @@
+import numpy as np
+import scipy
+from enum import IntEnum
+
+debris_category = IntEnum('Category', 'rb sc soc')
+from numba import njit, prange
+
+""" ----------------- Mean ----------------- """
+def make_mean_AM(debris_type):
+
+    def RB_mean_AM(lambda_c):
+
+        mean_am_1 = np.empty_like(lambda_c)
+        mean_am_2 = np.empty_like(lambda_c)
+
+        mean_am_1[lambda_c<=-0.5] = -0.45
+        I = (lambda_c>-0.5) & (lambda_c<0)
+        mean_am_1[I] = -0.45 - (0.9*(lambda_c[I] +0.5))
+        mean_am_1[lambda_c>=0] = -0.9
+
+        mean_am_2.fill(-0.9)
+
+        return np.array([mean_am_1,mean_am_2])
+
+    def SC_mean_AM(lambda_c):
+        mean_am_1 = np.empty_like(lambda_c)
+        mean_am_2 = np.empty_like(lambda_c)
+
+        mean_am_1[lambda_c<=-1.1] = -0.6
+        I = (lambda_c>-1.1) & (lambda_c<0)
+        mean_am_1[I] = -0.6 - (0.318*(lambda_c[I] +1.1))
+        mean_am_1[lambda_c>=0] = -0.95
+
+        mean_am_2[lambda_c<=-0.7] = -1.2
+        I = (lambda_c>-0.7) & (lambda_c<-0.1)
+        mean_am_2[I] = -1.2 - (1.333*(lambda_c[I] + 0.7))
+        mean_am_2[lambda_c>=-0.1] = -2.0
+
+        return np.array([mean_am_1,mean_am_2])
+
+    def SOC_mean_AM(lambda_c):
+
+        mean_am_1 = np.empty_like(lambda_c)
+        mean_am_2 = np.empty_like(lambda_c)
+
+        mean_am_1[lambda_c<=-1.75] = -0.3
+        I = (lambda_c>-1.75) & (lambda_c<-1.25)
+        mean_am_1[I] = -0.3 - (1.4*(lambda_c[I] +1.75))
+        mean_am_1[lambda_c>=-1.25] = -1.0
+
+        mean_am_2.fill(0)
+        return np.array([mean_am_1,mean_am_2])
+
+    if debris_type == debris_category.rb:
+        return RB_mean_AM
+    elif debris_type == debris_category.sc:
+        return SC_mean_AM
+    else:
+        return SOC_mean_AM
+
+""" ----------------- Standard Deviation ----------------- """
+
+def make_standard_dev_AM(debris_type):
+
+    def RB_std_dev_AM(lambda_c):
+
+        std_dev_1 = np.empty_like(lambda_c)
+        std_dev_2 = np.empty_like(lambda_c)
+
+        std_dev_1.fill(0.55)
+
+        std_dev_2[lambda_c<=-1.0] = 0.28
+        I = (lambda_c>-1.0) & (lambda_c<0.1)
+        std_dev_2[I] = 0.29 - (0.1636*(lambda_c[I] +1))
+        std_dev_2[lambda_c>=0.1] = 0.1
+
+        return np.array([std_dev_1,std_dev_1])
+
+
+    def SC_std_dev_AM(lambda_c):
+
+        std_dev_1 = np.empty_like(lambda_c)
+        std_dev_2 = np.empty_like(lambda_c)
+
+        std_dev_1[lambda_c<=-1.3] = 0.1
+        I = (lambda_c>-1.3) & (lambda_c<-0.3)
+        std_dev_1[I] = 0.1 + (0.2*(lambda_c[I] +1.3))
+        std_dev_1[lambda_c>=-0.3] = 0.3
+
+        std_dev_2[lambda_c<=-0.5] = 0.5
+        I = (lambda_c>-0.5) & (lambda_c<-0.3)
+        std_dev_2[I] = 0.5 - ((lambda_c[I] + 0.5))
+        std_dev_2[lambda_c>=-0.3] = 0.3
+
+        return np.array([std_dev_1,std_dev_1])
+
+
+    def SOC_std_dev_AM(lambda_c):
+        std_dev_1 = np.empty_like(lambda_c)
+        std_dev_2 = np.empty_like(lambda_c)
+
+        std_dev_1[lambda_c<=-3.5] = 0.2
+        I = (lambda_c>-3.5)
+        std_dev_1[I] = 0.2 + (0.1333*(lambda_c[I] +3.5))
+
+        std_dev_2.fill(0)
+
+        return np.array([std_dev_1,std_dev_1])
+
+    if debris_type == debris_category.rb:
+        return RB_std_dev_AM
+    elif debris_type == debris_category.sc:
+        return SC_std_dev_AM
+    else:
+        return SOC_std_dev_AM
+
+""" ----------------- Alpha ----------------- """
+def alpha_AM(lambda_c, debris_type):
+    def RB_alpha_AM(lambda_c):
+        alpha = 1
+        # dev1 rule
+        if lambda_c <= -1.4:
+            alpha = 1
+        elif (lambda_c > -1.4 and lambda_c < 0):
+            alpha = 1 - (0.3571*(lambda_c + 1.4))
+        else:
+            alpha = 0.5
+        return alpha
+
+    def SC_alpha_AM(lambda_c):
+        alpha = 1
+        # dev1 rule
+        if lambda_c <= -1.95:
+            alpha = 0
+        elif (lambda_c > -1.95 and lambda_c < 0.55):
+            alpha = 0.3 + (0.4*(lambda_c + 1.2))
+        else:
+            alpha = 1
+        return alpha
+
+    def SOC_alpha_AM(lambda_c):
+        # Is not used by SOC, for saftey returning 1
+        alpha = 1
+        return alpha
+
+    if debris_type == debris_category.rb:
+        return RB_alpha_AM(lambda_c)
+    elif debris_type == debris_category.sc:
+        return SC_alpha_AM(lambda_c)
+    else:
+        return SOC_alpha_AM(lambda_c)
+
+alpha_AM = np.vectorize(alpha_AM)
+
+""" ----------------- Distribution A/M ----------------- """
+def distribution_AM(lambda_c, debris_type):
+
+    N = len(lambda_c)
+    lambda_c = np.array(lambda_c)
+
+    mean_factory = make_mean_AM(debris_type)
+    std_dev_factor = make_standard_dev_AM(debris_type)
+
+    mean_preSwitch = np.array(mean_factory(lambda_c))
+    std_dev_preSwitch = np.array(std_dev_factor(lambda_c))
+
+    alpha = np.array(alpha_AM(lambda_c, debris_category.rb)) # This takes a long time
+    switch = np.random.uniform(0,1, N)
+
+    if debris_type == debris_category.rb or debris_type == debris_category.sc:
+
+        means = np.empty(N)
+        I,J = switch<alpha, switch>=alpha
+        means[I] = mean_preSwitch[0, I]
+        means[J] = mean_preSwitch[1, J]
+
+        devs = np.empty(N)
+        devs[I] = std_dev_preSwitch[0, I]
+        devs[J] = std_dev_preSwitch[1, J]
+
+        return np.random.normal(means, devs, N)
+
+    else:
+        means = mean_preSwitch[0]
+        devs = std_dev_preSwitch[0]
+
+        return np.random.normal(means, devs, N)
+
+""" ----------------- Area ----------------- """
+def avg_area(L_c):
+
+    A = np.copy(L_c)
+    I = A < 0.00167 #(m)
+    A[I] = 0.540424 * A[I]**2
+    I = A >= 0.00167 #(m)
+    A[I] = 0.556945 * A[I]**2.0047077
+
+    return A
+
+
+
+""" ----------------- Mean ----------------- """
+@njit()
+def mean_deltaV(kai, explosion):
+    if explosion == True:
+        return (0.2 * kai) + 1.85
+    else:
+        # Is a collision
+        return (0.9 * kai) + 2.9
+
+""" ----------------- Standard Deviation ----------------- """
+def std_dev_deltaV():
+    return 0.4
+
+""" ----------------- Distribution delta V ----------------- """
+@njit()
+def distriNormale(mu,sigma,x):
+    p = (1/(sigma*np.sqrt(2*np.pi))*np.exp(-1/2.*((x-mu)/sigma)**2))
+    return p
+
+@njit()    
+def distriDeltaVExpl(nu,chi):
+    mu = 0.2*chi + 1.85
+    return distriNormale(mu,0.4,nu)
+    
+@njit( parallel=True )
+def distribution_deltaV(chi, v_c, explosion=False):
+        N = len(chi)
+        result = np.empty_like(chi)
+        progress = 0
+
+        for i in prange(N):
+            mean = mean_deltaV(chi[i], explosion)
+            dev  = 0.4
+            x = np.random.rand()
+            dv =x*1.3*v_c
+            dist = distriDeltaVExpl(np.log10(dv),chi[i])
+            y = np.random.rand()
+            while y > dist:
+                x = np.random.rand()
+                dv = x*1.3*v_c
+                dist =  distriDeltaVExpl(np.log10(dv),chi[i])
+                y = np.random.rand()
+            result[i] = dist
+        return result
+
+#     N = len(chi)
+#     mean = mean_deltaV(chi, explosion)
+#     dev  = std_dev_deltaV()
+# # print(np.mean(mean),dev)
+#     max_itr = 15000
+#     i = 0
+#     base = 10
+#     centered = np.random.normal(0, dev, N)
+#     I = np.nonzero(base**(mean+centered)>1.3*v_c)[0]
+#     n = len(I)
+#     while n != 0 and i<max_itr:
+#         centered[I] = np.random.normal(0, dev, n)
+#         #I = np.nonzero(base**(mean+centered)>1.3*v_c)[0]
+#         J = np.nonzero(base**(mean[I] + centered[I])>1.3*v_c)[0]
+#         I = I[J]
+#         n = len(I)
+#         i+=1
+#     centered[I] = np.log10(1.3*v_c)
+#     result = base**centered
+#     return result
+
+""" ----------------- Unit vector delta V ----------------- """
+def unit_vector(N):
+    vectors = np.random.normal(0, 1, np.array([N, 3]))
+    vectors /= np.sqrt((vectors**2).sum(axis=1))[:, None]
+    return vectors
+
+def velocity_vectors(N, target_velocity, velocities):
+    unit_vectors = unit_vector(N)
+    velocity_vectors = velocities[:, None] * unit_vectors
+    return target_velocity + velocity_vectors
+
+from numpy.random import uniform
+
+""" ----------------- Num. Fragments & Char. Length----------------- """
+def number_fragments(l_characteristic, m_target, m_projectile, v_impact, is_catastrophic, debris_type, explosion):
+    # Defining reference Mass
+    if explosion == True: # Can be multiplied by scaling factor S
+        print("explosion")
+        return 6*(l_characteristic)**(-1.6)
+    else:
+        m_ref = 0
+        if is_catastrophic:
+            m_ref = m_target + m_projectile
+        else:
+            m_ref = m_projectile * (v_impact)**2
+        return 0.1 * ((m_ref)**0.75) * l_characteristic**(-1.71)
+
+
+def characteristic_lengths(m_target, m_projectile, v_impact, is_catastrophic, debris_type, explosion):
+    bins = np.geomspace(0.001, 1, 100)
+    N_fragments = number_fragments(bins, m_target, m_projectile, v_impact, is_catastrophic, debris_type, explosion)
+    N_per_bin = np.array(N_fragments[:-1] - N_fragments[1:]).astype(int)
+    L_c = np.concatenate([uniform(bins[i], bins[i+1], size=N_per_bin[i]) for i in range(len(bins) - 1)])
+
+    return L_c
+
+def fragmentation(m_target, m_projectile, v_impact, is_catastrophic, debris_type, explosion):
+
+    prelim_L_c = characteristic_lengths(m_target, m_projectile, v_impact, is_catastrophic, debris_type, explosion)
+    prelim_lambda_c = np.log10(prelim_L_c)
+    prelim_areas = avg_area(prelim_L_c)
+    prelim_AM = np.array(distribution_AM(prelim_lambda_c, debris_type))
+    prelim_masses = prelim_areas / 10**prelim_AM
+
+    if explosion == True:
+
+        unaccounted_mass = m_target - np.sum(prelim_masses)
+
+        n_large_deb = np.random.randint(2, 8) # Pick 2-8 pieces of deb > 1m to spread out the rest of the mass
+
+       # Using 10**-4 to enure endpoints are not included
+        mass_range = np.linspace(10**-4, (unaccounted_mass - 10**-4), 10**4) # Create mass range, will use `n_large_deb` to split into sections
+        ranges = np.sort(np.random.choice(mass_range, n_large_deb - 1, replace=False))
+        ranges = np.concatenate([[0],ranges,[unaccounted_mass]])
+
+        # Note adding zero for subtraction to work (correct dims) then dropping it afterward
+        mass_per_deb = np.concatenate((ranges[1:],np.zeros(1))) - ranges
+        mass_per_deb = np.resize(mass_per_deb, mass_per_deb.size - 1)
+
+
+        # For L_c > 1, A/M Distribution is basically deterministic, therefore will just use avg value, can get using np.inf
+        assumed_AM_factory = make_mean_AM(debris_type)
+        assumed_len = np.ones(mass_per_deb.shape)
+        assumed_AM = assumed_AM_factory(assumed_len)
+
+        # Each mean has two possible values, randomly pick one of them for each piece of deb
+        AM_choices = np.random.choice([0,1], len(mass_per_deb), replace=True)
+        assumed_AM = 10**np.array([assumed_AM[AM_choices[i], i] for i in range(assumed_AM.shape[1])])
+
+        # mass * AM = A(L_c), therefore can reverse Area function for L_c
+        area = mass_per_deb * assumed_AM
+        found_L_c = np.sort((area / 0.556945)**(1/2.0047077)) # Inversing the Area function defined above
+        found_lambda_c = np.log10(found_L_c)
+        found_areas = avg_area(found_L_c)
+
+        found_AM = np.array(distribution_AM(found_lambda_c, debris_type))
+        found_masses = found_areas / assumed_AM # Using assumed A/M since A/M is a distribution and could get diff values.
+
+        L_c = np.concatenate([prelim_L_c, found_L_c])
+        areas = np.concatenate([prelim_areas, found_areas])
+        masses = np.concatenate([prelim_masses, found_masses])
+        AM = np.concatenate([prelim_AM, assumed_AM])
+
+        return L_c, areas, masses, AM
+    else:
+        # Is a collision
+        if is_catastrophic == True:
+
+            unaccounted_mass = (m_target + m_projectile) - np.sum(prelim_masses)
+            # Put the rest of the mass in many fragments in last bin
+            deposit_bin = (np.geomspace(0.001, 1, 100)[-1] + np.geomspace(0.001, 1, 100)[-2])/2
+
+
+            n_large_deb = np.random.randint(15, 50) # Pick 2-8 pieces of deb > 1m to spread out the rest of the mass
+
+           # Using 10**-4 to enure endpoints are not included
+            mass_range = np.linspace(10**-4, (unaccounted_mass - 10**-4), 10**4) # Create mass range, will use `n_large_deb` to split into sections
+            ranges = np.sort(np.random.choice(mass_range, n_large_deb - 1, replace=False))
+            ranges = np.concatenate([[0],ranges,[unaccounted_mass]])
+
+            # Note adding zero for subtraction to work (correct dims) then dropping it afterward
+            found_masses = np.concatenate((ranges[1:],np.zeros(1))) - ranges
+            found_masses = np.resize(found_masses, found_masses.size - 1)
+
+            found_L_c = np.ones_like(found_masses) * deposit_bin
+            found_areas = avg_area(found_L_c)
+            found_AM = found_areas / found_masses
+
+            L_c = np.concatenate([prelim_L_c, found_L_c])
+            areas = np.concatenate([prelim_areas, found_areas])
+            masses = np.concatenate([prelim_masses, found_masses])
+            AM = np.concatenate([prelim_AM, found_AM])
+
+            return L_c, areas, masses, AM
+
+        else:
+            # Is a non catastrophic collision, Deposit remaining mass in 1 large piece of deb
+            unaccounted_mass = np.array([(m_target + m_projectile) - np.sum(prelim_masses)])
+
+            # For L_c > 1, A/M Distribution is basically deterministic, therefore will just use avg value, can get using np.inf
+            assumed_AM_factory = make_mean_AM(debris_type)
+            assumed_len = np.ones(unaccounted_mass.shape)
+            assumed_AM = assumed_AM_factory(assumed_len)
+
+            # Each mean has two possible values, randomly pick one of them for each piece of deb
+            AM_choices = np.random.choice([0,1], len(unaccounted_mass), replace=True)
+            assumed_AM = 10**np.array([assumed_AM[AM_choices[i], i] for i in range(assumed_AM.shape[1])])
+
+             # mass * AM = A(L_c), therefore can reverse Area function for L_c
+            area = unaccounted_mass * assumed_AM
+            found_L_c = np.sort((area / 0.556945)**(1/2.0047077)) # Inversing the Area function defined above
+            found_lambda_c = np.log10(found_L_c)
+            found_areas = avg_area(found_L_c)
+
+            L_c = np.concatenate([prelim_L_c, found_L_c])
+            areas = np.concatenate([prelim_areas, found_areas])
+            masses = np.concatenate([prelim_masses, unaccounted_mass])
+            AM = np.concatenate([prelim_AM, assumed_AM])
+
+            return L_c, areas, masses, AM
